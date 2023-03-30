41bfdaac71fa00a2bf8a66618c02057be9f6db97
alalading0207 <Dingyulin@whu.edu.cn>
2023-03-30 21:40:43 +0800

    first commit

--- datasets/SARBuD_dataset.py
+++ datasets/SARBuD_dataset.py
@@ -2,26 +2,36 @@ import os
 import numpy as np
 import cv2
 import torch
-
+ 
 class DARBuDDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms):
         self.root = root
         self.item_list = list(open(os.path.join(root, "list.txt"), 'r').readlines())
         self.transforms = transforms
+
     def __getitem__(self, index):
         image = self.read_tif(os.path.join(self.root, "images", self.item_list[index].strip()))
-        target = torch.max(self.read_tif(os.path.join(self.root, "Labelimgs", self.item_list[index].strip())) > 0, 0, True)[0].float()
-        image, target = torch.split(self.transforms(torch.concat([image, target], 0)), [3, 1], 0)
+        target = torch.max(self.read_tif(os.path.join(self.root, "Labelimgs", self.item_list[index].strip())) > 0, 0, True)[0].float() # target是0./1.
+        
+        image = self.transforms(image)  # 影像归一化
+        # image, target = torch.split(self.transforms(torch.concat([image, target], 0)), [3, 1], 0)
+
         downsample_boundary_2 = self.get_downsample_boundary(target, [2,2], [2,2], 0.5)
         downsample_boundary_4 = self.get_downsample_boundary(target, [4,4], [4,4], 0.25)
         downsample_boundary_8 = self.get_downsample_boundary(target, [8,8], [8,8], 0.125)
+
         return image, target, downsample_boundary_2, downsample_boundary_4, downsample_boundary_8
 
     def __len__(self):
         return len(self.item_list)
     
     def read_tif(self, path):
-        return torch.Tensor(cv2.imread(path, 1).transpose((2,0,1)).astype(np.float32))
+        # image1 = cv2.imread(path, 1)      # unit8    256,256,3
+        # image2 = image1.transpose((2,0,1))      # unit8    3,256,256
+        # image3 = image2.astype(np.float32)      # float32  3,256,256
+        # image4 = torch.Tensor(image3)
+        return torch.Tensor(cv2.imread(path, 0)[:,:,None].transpose((2,0,1)).astype(np.float32))
+        # return torch.Tensor(cv2.imread(path, 1).transpose((2,0,1)).astype(np.float32))
     
     def get_downsample_boundary(self, input, kernel_size, stride, lower_bound, upper_bound=1):
         boundary = torch.nn.functional.avg_pool2d(input, kernel_size, stride)
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import argparse
 import datetime
 import torch
 from torchvision import transforms
-from torchsummary import summary
+# from torchsummary import summary
 import matplotlib.pyplot as plt
 from datasets.SARBuD_dataset import DARBuDDataset
 from models import get_model
@@ -15,27 +15,18 @@ logger = None
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a segmentor')
-    parser.add_argument(
-        '--ckpt-dir', type=str, default='./checkpoints/')
-    parser.add_argument(
-        '--load-from', help='the checkpoint file to load weights from')
-    parser.add_argument('--log-dir', type=str, default="./logs/")
-    parser.add_argument(
-        '--gpu-id',
-        type=int,
-        default=0,
-        help='id of gpu to use '
-        '(only applicable to non-distributed training)')
-    parser.add_argument(
-        '--deterministic',
-        action='store_true',
-        help='whether to set deterministic options for CUDNN backend.')
-    parser.add_argument('--epoch', type=int, default=16)
-    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--ckpt-dir', type=str, default='/gemini/code/segmentation0329/checkpoint')
+    parser.add_argument('--load-from', help='the checkpoint file to load weights from')
+    parser.add_argument('--log-dir', type=str, default="/gemini/code/segmentation0329/logs")
+    parser.add_argument('--gpu-id', type=int, default=0, help='id of gpu to use ' '(only applicable to non-distributed training)')
+    parser.add_argument('--deterministic', action='store_true', help='whether to set deterministic options for CUDNN backend.')
+    parser.add_argument('--epoch', type=int, default=20)
+    parser.add_argument('--batch-size', type=int, default=2)
     parser.add_argument('--model', type=str, default='unet')
-    parser.add_argument('--dataset', type=str, default='./')
+    parser.add_argument('--dataset', type=str, default='/gemini/code/dataset')
 
     return parser.parse_args()
+
 
 def train(args, model, optimizer, bce_loss, cbl_loss, train_dataloader, val_dataloader, epoch, metric):
     optimizer.zero_grad()
@@ -52,18 +43,24 @@ def train(args, model, optimizer, bce_loss, cbl_loss, train_dataloader, val_data
             label_1_4 = label_1_4.cuda()
             label_1_8 = label_1_8.cuda()
             logits, cbl_1_8, bce_1_8, cbl_1_4, bce_1_4, cbl_1_2, bce_1_2 = model(input)
+            
             total_loss = \
                 bce_loss(torch.sigmoid(logits), label) + \
-                cbl_loss(cbl_1_8, label_1_8) + \
-                cbl_loss(cbl_1_4, label_1_4) + \
-                cbl_loss(cbl_1_2, label_1_2) + \
-                bce_loss(torch.sigmoid(bce_1_8), label_1_8) + \
-                bce_loss(torch.sigmoid(bce_1_4), label_1_4) + \
-                bce_loss(torch.sigmoid(bce_1_2), label_1_2) 
+                0.1*cbl_loss(cbl_1_8, label_1_8) + \
+                0.1*cbl_loss(cbl_1_4, label_1_4) + \
+                0.1*cbl_loss(cbl_1_2, label_1_2) + \
+                0.5*bce_loss(torch.sigmoid(bce_1_8), label_1_8) + \
+                0.5*bce_loss(torch.sigmoid(bce_1_4), label_1_4) + \
+                0.5*bce_loss(torch.sigmoid(bce_1_2), label_1_2) 
             total_loss = total_loss.mean()
+
+            cblloss = cbl_loss(cbl_1_2, label_1_2)
+
             logger.info("Epoch[{}/{}] batch[{}] loss: {}".format(i, epoch, idx, total_loss))
+            logger.info("Epoch[{}/{}] batch[{}] cblloss: {}".format(i, epoch, idx, cblloss))
             total_loss.backward()
             optimizer.step()
+
         new_acc = test(model, bce_loss, cbl_loss, val_dataloader, metric)
         if new_acc > acc:
             torch.save({
@@ -86,12 +83,12 @@ def test(model, bce_loss, cbl_loss, dataloadder, metric):
         logits, cbl_1_8, bce_1_8, cbl_1_4, bce_1_4, cbl_1_2, bce_1_2 = model(input)
         total_loss = \
             bce_loss(torch.sigmoid(logits), label) + \
-            cbl_loss(cbl_1_8, label_1_8) + \
-            cbl_loss(cbl_1_4, label_1_4) + \
-            cbl_loss(cbl_1_2, label_1_2) + \
-            bce_loss(torch.sigmoid(bce_1_8), label_1_8) + \
-            bce_loss(torch.sigmoid(bce_1_4), label_1_4) + \
-            bce_loss(torch.sigmoid(bce_1_2), label_1_2) 
+            0.1*cbl_loss(cbl_1_8, label_1_8) + \
+            0.1*cbl_loss(cbl_1_4, label_1_4) + \
+            0.1*cbl_loss(cbl_1_2, label_1_2) + \
+            0.5*bce_loss(torch.sigmoid(bce_1_8), label_1_8) + \
+            0.5*bce_loss(torch.sigmoid(bce_1_4), label_1_4) + \
+            0.5*bce_loss(torch.sigmoid(bce_1_2), label_1_2) 
     if metric:
         logger.info("Testing acc: {}".format(metric))
     return 0.1
@@ -101,6 +98,7 @@ def main():
     args = parse_args()
     if not os.path.exists(args.log_dir):
         os.mkdir(args.log_dir)
+
     global logger 
     logger = get_logger(args.log_dir + "/{}-{}-{}-{}.csv".format(
         args.model, 
@@ -116,14 +114,17 @@ def main():
     # init dataloader
     train_transform = transforms.Compose([
         transforms.Resize([256, 256]),
-        transforms.Normalize([0, 0, 0, 0], [255, 255, 255, 255]),
+        transforms.Normalize([0], [255]),
+        transforms.Normalize(0.5, 0.5),
         # transforms.RandomCrop([256, 256])
     ])
     val_transform = transforms.Compose([
         transforms.Normalize([0, 0, 0, 0], [255, 255, 255, 255])])
+    
     train_dataloader = torch.utils.data.DataLoader(DARBuDDataset(args.dataset + "/train/", train_transform), batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=4)
     val_dataloader = torch.utils.data.DataLoader(DARBuDDataset(args.dataset + "/val/", val_transform), batch_size=args.batch_size, shuffle=False, drop_last=False)
     test_dataloader = torch.utils.data.DataLoader(DARBuDDataset(args.dataset + "/test/", val_transform), batch_size=args.batch_size, shuffle=False, drop_last=False)
+    
     # init model
     model = get_model(args.model).cuda()
     # optimizer
@@ -137,6 +138,7 @@ def main():
     # losses
     bce_loss = torch.nn.BCELoss()
     cbl_loss = CBLLoss([3,3], 10)
+
     # metrics
     metric = None
     train(args, model, optimizer, bce_loss, cbl_loss, train_dataloader, val_dataloader, args.epoch, metric)
